179b4d1fe0a4d284b4ec0558d68c6b41c22db78c
ajikowiyu <ajikowiyu@gmail.com>
2026-02-27 21:59:24 +0700

    list produk

--- controller/c_master_produk.js
+++ controller/c_master_produk.js
@@ -1,6 +1,10 @@
+const m_produk = require('../model/m_produk')
+
 module.exports =
 {
-    index: function (req,res) {
-        res.render('master-produk/main')
+    index: async function (req,res) {
+        res.render('master-produk/main', {
+            data_produk: await m_produk.get_semua_produk()
+        })
     },
 }
