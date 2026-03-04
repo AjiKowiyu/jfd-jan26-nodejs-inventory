77bf4f0ac6a82a1d2d1ab34aafab4c28be7b8c83
ajikowiyu <ajikowiyu@gmail.com>
2026-03-04 20:21:49 +0700

    express validator

--- controller/c_master_produk.js
+++ controller/c_master_produk.js
@@ -1,4 +1,14 @@
+const {body, query, validationResult} = require('express-validator')
 const m_produk = require('../model/m_produk')
+const { max } = require('moment')
+
+
+let validasi_insertProduk = [
+    body('form_kode_barang')
+    .notEmpty().withMessage('Kode Barang tidak boleh kosong')
+    .isAlphanumeric().withMessage('Kode Barang hanya menerima Huruf & Angka')
+    .isLength({min:5, max: 10}).withMessage('Kode Barang maksimal 10 karakter')
+]
 
 module.exports =
 {
@@ -19,7 +29,20 @@ module.exports =
 
 
 
+    validasi_insertProduk,
+
+
+
     insert: async function (req,res) {
+        let validasi = validationResult(req)
+        // jika validasi gagal
+        if (validasi.errors.length > 0) {
+            return res.render('master-produk/form-tambah', {
+                req: req,
+                pesan_validasi_error: validasi.array(),
+            })
+        }
+
         try {
             let proses_insert = await m_produk.insert_1_produk(req)
             if (proses_insert.affectedRows > 0) {
